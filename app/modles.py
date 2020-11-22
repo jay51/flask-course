9a2de4e8fdd29cf3f54cee5470946afad6cd3a25
jay51 <nenojaafar3@gmail.com>
2020-11-22 05:11:11 +0000

    design the DB schema for comments and follows

--- app/modles.py
+++ app/modles.py
@@ -9,12 +9,16 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(1000))
     role = db.Column(db.Integer, default=1)
 
+    # get all the teams through a secondary table that stores id for a user and id for a team
+    teams = db.relationship("Team", secondary="Follow")
+
     def __repl__(self):
         return "<User {username}, {email}, {role}>".format(
             username=self.username,
             email=self.email,
             role=self.role,
         )
+
 
 class Conference(db.Model):
     __tablename__ = "conferences"
@@ -43,6 +47,9 @@ class Team(db.Model):
     # each team will have one conference, foreignkey to conference
     conference_id = db.Column(db.Integer, db.ForeignKey("conferences.id"))
 
+    # get all users part of this team through a secondary table that stores id for a user and id for a team
+    users = db.relationship("User", secondary="Follow")
+
     def __repr__(self):
         return "<Team {school}, {mascot}, {abbreviation} {logos} {conference_id}>".format(
             school          = self.school,
@@ -52,3 +59,27 @@ class Team(db.Model):
             conference_id   = self.conference_id,
         )
 
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    comment = db.Column(db.String, nullable=False)
+
+    def __repl__(self):
+        return "<Comment {user_id}, {team_id}, {comment}>".format(
+            user_id=self.user_id,
+            team_id=self.team_id,
+            comment=self.comment,
+        )
+
+
+class Follow(db.Model):
+    __tablename__ = "follows"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+
+    def __repl__(self):
+        return "<Follow {user_id}, {team_id}>".format( user_id=self.user_id, team_id=self.team_id)
