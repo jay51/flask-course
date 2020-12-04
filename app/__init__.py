9ae6ae7821762603ae9a1cdd5dd498e954278ea4
jay51 <nenojaafar3@gmail.com>
2020-12-04 02:07:27 +0000

    deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,9 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__, template_folder="./web/templates")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://jay:pass@localhost:5432/itse2302"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://jay:pass@localhost:5432/itse2302"
+db_url = "postgres://oyztumubfnncek:7829667524f45e6c4589dbccc25358202e2e405bf9261ae1dfccbcd6a19a970a@ec2-3-220-98-137.compute-1.amazonaws.com:5432/dc9r7cjpg0nedg"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 # app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "HALO"
 db = SQLAlchemy(app)
